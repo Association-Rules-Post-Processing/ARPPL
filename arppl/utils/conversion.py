0ceb5ec261ef82bc02e173b9e47dd3fa9000703b
luizcintra <luizcintradev@gmail.com>
2022-09-01 12:06:10 -0300

    feat: export groups

--- arppl/utils/conversion.py
+++ arppl/utils/conversion.py
@@ -1,4 +1,7 @@
 import re
+import xlsxwriter
+from itertools import groupby
+import math
 
 from arppl import models
 
@@ -25,10 +28,76 @@ def split_antecedent(antecedent):
 
 
 def create_rules_from_row(row):
-    measures = {'support': models.Measure('support', row.support),
-                'confidence': models.Measure('confidence', row.confidence),
-                'lift': models.Lift(row.lift)}
-
     antecedent = split_antecedent(row.antecedent)
 
-    return models.Rule(antecedent, row.consequent, measures)
+    return models.Rule(
+        antecedent,
+        row.consequent,
+        _get_treated_measure_value_from_row(row.support, models.Measure),
+        _get_treated_measure_value_from_row(row.confidence, models.Measure),
+        _get_treated_measure_value_from_row(row.lift, models.MeasureIndependentlyAtOne),
+        _get_treated_measure_value_from_row(row.conviction, models.MeasureIndependentlyAtOne),
+        _get_treated_measure_value_from_row(row.hyperConfidence, models.Measure),
+        _get_treated_measure_value_from_row(row.cosine, models.Measure),
+        _get_treated_measure_value_from_row(row.chiSquare, models.Measure),
+        _get_treated_measure_value_from_row(row.coverage, models.Measure),
+        _get_treated_measure_value_from_row(row.doc, models.Measure),
+        _get_treated_measure_value_from_row(row.gini, models.Measure),
+        _get_treated_measure_value_from_row(row.hyperLift, models.Measure)
+    )
+
+
+def _get_treated_measure_value_from_row(value, measure_class):
+    if math.isnan(value):
+        return None
+    elif math.isinf(value):
+        return measure_class(math.inf)
+    else:
+        return measure_class(value)
+
+
+def export_groups_to_xlsx(directory, filename, groups):
+    workbook = xlsxwriter.Workbook(directory + filename)
+
+    sorted_groups = sorted(groups, key=lambda x: x.name)
+    groups_by_name = groupby(sorted_groups, key=lambda x: x.name)
+
+    for key, gps in groups_by_name:
+        worksheet = workbook.add_worksheet('Grupo ' + key)
+        worksheet.write('A1', 'Regras')
+        worksheet.write('B1', 'Suporte')
+        worksheet.write('C1', 'Confiança')
+        worksheet.write('D1', 'Lift')
+        worksheet.write('E1', 'Convicção')
+        worksheet.write('F1', 'Hiper Confiança')
+        worksheet.write('G1', 'Cosseno')
+        worksheet.write('H1', 'X²')
+        worksheet.write('I1', 'Cobertura')
+        worksheet.write('J1', 'Doc')
+        worksheet.write('K1', 'Gini')
+        worksheet.write('L1', 'Hiper Lift')
+
+        i = 2
+        for group in gps:
+            for r in group.rules:
+                worksheet.write('A' + str(i), r.to_string())
+                _write_value_in_xlsx('B' + str(i), r.support.value, worksheet)
+                _write_value_in_xlsx('C' + str(i), r.confidence.value, worksheet)
+                _write_value_in_xlsx('D' + str(i), r.lift.value, worksheet)
+                _write_value_in_xlsx('E' + str(i), r.conviction.value, worksheet)
+                _write_value_in_xlsx('F' + str(i), r.hyper_confidence.value, worksheet)
+                _write_value_in_xlsx('G' + str(i), r.cosine.value, worksheet)
+                _write_value_in_xlsx('H' + str(i), r.chi_square.value, worksheet)
+                _write_value_in_xlsx('I' + str(i), r.coverage.value, worksheet)
+                _write_value_in_xlsx('J' + str(i), r.doc.value, worksheet)
+                _write_value_in_xlsx('K' + str(i), r.gini.value, worksheet)
+                _write_value_in_xlsx('L' + str(i), r.hyper_lift.value, worksheet)
+                i += 1
+            i += 1
+    workbook.close()
+
+
+def _write_value_in_xlsx(cell, value, worksheet):
+    if value:
+        worksheet.write(cell, 'Inf' if math.isinf(value) else value)
+
